ac31ea06ee93875047312e0a07eaa73f69fb17d5
Alexsey <haost@yandex.ru>
2021-09-25 16:28:19 +0300

    Clearn

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
 import 'assets/scss/App.scss';
-<<<<<<< HEAD
 import Home from 'components/Home';
-=======
->>>>>>> a2ca32b80a516e0c6d8abedb99495aa16020294d
 import AboutPage from 'components/AboutPage';
 import WelcomePage from 'components/WelcomePage';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
@@ -14,7 +11,6 @@ class App extends React.Component {
     return (
       <BrowserRouter>
         <div className="app">
-<<<<<<< HEAD
           <Home/>
           {/* <nav>
             <Link to="/">Home</Link> | <Link to="/about">About</Link>
@@ -22,14 +18,7 @@ class App extends React.Component {
           {/* <hr /> */}
           {/* <Route exact path="/" component={WelcomePage} />
           <Route path="/about" component={AboutPage} /> */}
-=======
-          <nav>
-            <Link to="/">Home</Link> | <Link to="/about">About</Link>
-          </nav>
-          <hr />
-          <Route exact path="/" component={WelcomePage} />
-          <Route path="/about" component={AboutPage} />
->>>>>>> a2ca32b80a516e0c6d8abedb99495aa16020294d
+
         </div>
       </BrowserRouter>
     );
